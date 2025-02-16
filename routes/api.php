d05f3b664c0abd254df95262c699b9de718236ec
devosamagasser <devosamagasser@gmail.com>
2025-02-16 19:29:48 +0200

    google auth

--- routes/api.php
+++ routes/api.php
@@ -29,14 +29,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware('guest')->group(function () {
-    Route::post('auth/register', [AuthController::class, 'register']);
-    Route::post('auth/login', [AuthController::class, 'login']);
-    Route::post('otp-verification', [EmailVerificationController::class, 'otpVerification']);
-    Route::post('resend-otp', [EmailVerificationController::class, 'reSendOtp']);
-    Route::get('auth/{provider}/redirect', [SocialLoginController::class, 'redirect']);
-    Route::get('auth/{provider}/callback', [SocialLoginController::class, 'callBack']);
-});
+Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('otp-verification', [EmailVerificationController::class, 'otpVerification']);
+Route::post('resend-otp', [EmailVerificationController::class, 'reSendOtp']);
+Route::get('auth/{provider}/redirect', [SocialLoginController::class, 'redirect']);
+Route::get('auth/{provider}/callback', [SocialLoginController::class, 'callBack']);
 
 Route::middleware(['auth:sanctum','verified'])->group(function () {
     Route::delete('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
